67e55c55636bc134b96f509198ebb2a404a64d3c
erhangudinci <multimusikern@hotmail.com>
2015-12-15 13:58:52 +0100

    lab6

---
+++ Lab6/MyDoubleType.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class MyDoubleType
+    {
+        public double DoubleValue { get; set; }
+
+        public MyDoubleType(double doubleValue)
+        {
+            DoubleValue = doubleValue;
+        }
+
+        public override string ToString() => $"DoubleValue: {DoubleValue}";
+
+        public static MyDoubleType operator +(MyDoubleType myDoubleType, double tal)
+        {
+            ;
+            return new MyDoubleType(myDoubleType.DoubleValue + tal);
+        }
+
+        public static MyDoubleType operator +(double tal, MyDoubleType myDoubleType)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue + tal); 
+        }
+        public static MyDoubleType operator +(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            ;
+            return new MyDoubleType(myDoubleType.DoubleValue + myDoubleTypes.DoubleValue); 
+        }
+
+        public static MyDoubleType operator -(MyDoubleType myDoubleType, double tal)
+        {
+            ;
+            return new MyDoubleType(myDoubleType.DoubleValue - tal); 
+        }
+
+        public static MyDoubleType operator -(double tal, MyDoubleType myDoubleType)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue - tal); 
+        }
+        public static MyDoubleType operator -(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue - myDoubleTypes.DoubleValue); ;
+        }
+        public static MyDoubleType operator *(MyDoubleType myDoubleType, double tal)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue * tal); ;
+        }
+
+        public static MyDoubleType operator *(double tal, MyDoubleType myDoubleType)
+        {
+            ;
+            return new MyDoubleType(myDoubleType.DoubleValue * tal); ;
+        }
+        public static MyDoubleType operator *(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue * myDoubleTypes.DoubleValue); ;
+        }
+        public static MyDoubleType operator /(MyDoubleType myDoubleType, double tal)
+        {
+            return new MyDoubleType(myDoubleType.DoubleValue / tal);
+        }
+
+        public static MyDoubleType operator /(double tal, MyDoubleType myDoubleType)
+        {
+            return new MyDoubleType(myDoubleType.DoubleValue / tal);
+        }
+        public static MyDoubleType operator /(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            
+            return new MyDoubleType(myDoubleType.DoubleValue / myDoubleTypes.DoubleValue);
+        }
+        public static bool operator ==(MyDoubleType myDoubleType, double tal)
+        {
+            if(myDoubleType.DoubleValue == tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue == tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator ==(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue == myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(MyDoubleType myDoubleType, double tal)
+        {
+            if (myDoubleType.DoubleValue != tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue != tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue != myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(MyDoubleType myDoubleType, double tal)
+        {
+            if (myDoubleType.DoubleValue < tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue < tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue == myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >(MyDoubleType myDoubleType, double tal)
+        {
+            if (myDoubleType.DoubleValue < tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue < tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue == myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >=(MyDoubleType myDoubleType, double tal)
+        {
+            if (myDoubleType.DoubleValue <= tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >=(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue > tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >=(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue == myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <=(MyDoubleType myDoubleType, double tal)
+        {
+            if (myDoubleType.DoubleValue >= tal)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <=(double tal, MyDoubleType myDoubleType)
+        {
+            if (myDoubleType.DoubleValue >= tal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <=(MyDoubleType myDoubleType, MyDoubleType myDoubleTypes)
+        {
+            if (myDoubleType.DoubleValue == myDoubleTypes.DoubleValue)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uppgift1();
+            //Uppgift3();
+            MyDoubleType mdt1 = new MyDoubleType(5);
+
+            MyDoubleType mdt2 = mdt1 + 5;
+
+            MyDoubleType mdt3 = mdt1 + mdt2;
+
+            MyDoubleType mdt4 = new MyDoubleType(5);
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
+
+                 mdt1, mdt2, mdt3, mdt1 > mdt2, mdt1 == mdt4);
+        }
+
+        private static void Uppgift3()
+        {
+            Point point = new Point(3, 4);
+            Console.WriteLine(point);
+            Point3D point3D = new Point(3, 2);
+            Console.WriteLine(point3D);
+        }
+
+        private static void Uppgift1()
+        {
+            float firstFloat = 0.3f;
+            long firstLong = -123234L;
+            char firstChar = 'a';
+            string firstSTring = "hej";
+            ulong firstULong = 38532469576957325L;
+        }
+    }
+}
